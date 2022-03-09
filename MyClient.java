fea7c3d1395897b8ee1b70e18131d910fd79ce3b
Kurtis <kurtis.fraser@students.mq.edu.au>
2022-03-09 19:23:37 +1100

    Added printWriter reminder comment

--- MyClient.java
+++ MyClient.java
@@ -5,7 +5,7 @@ public class MyClient {
         try {
             Socket s = new Socket("localhost", 6666);
             DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-            dout.writeUTF("Hello Server");
+            dout.writeUTF("Hello Server"); //Doesn't work with ds use printWriter instead
             dout.flush();
             dout.close();
         } catch (Exception e) {
