8ddbe0c889d4572988e3f2f779ad1cb450e64058
Kurtis <kurtis.fraser@students.mq.edu.au>
2022-03-22 14:06:09 +1100

    Workshop4 task 2.2

--- MyClient.java
+++ MyClient.java
@@ -3,10 +3,35 @@ import java.net.*;
 public class MyClient {
     public static void main(String[] args) {
         try {
-            Socket s = new Socket("localhost", 6666);
+            Socket s = new Socket("localhost", 50000);
+            // DataInputStream din = new DataInputStream(s.getInputStream());
             DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-            dout.write(("Hello Server\n").getBytes());
+            BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            //HELO
+            dout.write(("HELO").getBytes());
+            // String str = (String)br.readLine();
+            // System.out.println("Received: " + str);
             dout.flush();
+
+            //Auth
+            String username = System.getProperty("user.name");
+            dout.write(("AUTH "+username).getBytes());
+            // str = (String)br.readLine();
+            // System.out.println("Received: " + str);
+
+            dout.flush();
+
+            //Read xml and ready
+
+            dout.write(("REDY").getBytes());
+            // str = (String)br.readLine();
+            // System.out.println("Received: " + str);
+            dout.flush();
+
+            dout.write(("QUIT").getBytes());
+            // str = (String)br.readLine();
+            // System.out.println("Received: " + str);
+            
             dout.close();
             s.close();
         } catch (Exception e) {
