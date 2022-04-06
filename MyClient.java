a037ae17ff7fef4f61b19534c952e0a9a79582f9
Kurtis <kurtis.fraser@students.mq.edu.au>
2022-04-06 17:20:14 +1000

    Fixed Bug with running multiple jobs

--- MyClient.java
+++ MyClient.java
@@ -26,56 +26,62 @@ public class MyClient {
 
             while(!response.contains("NONE")){
 
-            // REDY
-            dout.write(("REDY\n").getBytes());
-            response = din.readLine();
-            System.out.println("Response REDY: " + response);
-            dout.flush();
-
-            if(response.contains("JOBN")){
-            // GETS
-            String job[] = response.split(" ");
-            // System.out.println("Job[] " + job[0]);
-            while(!response.contains("DATA")){
-                dout.write(("GETS Capable "+job[4]+" "+job[5]+" "+job[6]+"\n").getBytes());
+                // REDY
+                dout.write(("REDY\n").getBytes());
                 response = din.readLine();
-                System.out.println("Response: " + response);
+                System.out.println("Response REDY: " + response);
                 dout.flush();
-            }
-            
-            // OK
-            ArrayList<String> servers = new ArrayList<String>();
-            dout.write(("OK\n").getBytes());
-            String data[] = response.split(" ");
-            for(int a=0; a<Integer.parseInt(data[1]); a++){
-                response = din.readLine();
-                System.out.println("A: " + data[1]);
-                System.out.println("Response in loop: " + response);
-                servers.add(response);
-            }
-            dout.flush();
 
-            System.out.println("Confirm after server loop");
+                
 
-            //Confirm server names received
-            dout.write(("OK\n").getBytes());
-            dout.flush();
+                if(response.contains("JOBN")){
+                    // GETS
+                    String job[] = response.split(" ");
+                    // System.out.println("Job[] " + job[0]);
+                    // while(!response.contains("DATA")){
+                        dout.write(("GETS Capable "+job[4]+" "+job[5]+" "+job[6]+"\n").getBytes());
+                        response = din.readLine();
+                        System.out.println("Response: " + response);
+                        dout.flush();
+                    // }
+                    
+                    // OK
+                    ArrayList<String> servers = new ArrayList<String>();
+                    dout.write(("OK\n").getBytes());
+                    String data[] = response.split(" ");
+                    for(int a=0; a<Integer.parseInt(data[1]); a++){
+                        response = din.readLine();
+                        System.out.println("A: " + data[1]);
+                        System.out.println("Response in loop: " + response);
+                        servers.add(response);
+                    }
+                    dout.flush();
 
-            //Find largest and schedule it
-            //Determine largest server based on core count which is 4 for joon 0 and 16 for super-silk
-            String largestServer = servers.get(0);
-            // System.out.println("Servers Check " + servers.toString());
-            for(String serv: servers){
-                if(Integer.parseInt(serv.split(" ")[4]) > Integer.parseInt(largestServer.split(" ")[4]) && serv.split(" ")[2].equals("inactive")){
-                    System.out.println("New Largest: " + serv);
-                    largestServer = serv;
+                    System.out.println("Confirm after server loop");
+
+                    //Confirm server names received
+                    dout.write(("OK\n").getBytes());
+                    dout.flush();
+
+                    //Find largest and schedule it
+                    //Determine largest server based on core count which is 4 for joon 0 and 16 for super-silk
+                    String largestServer = servers.get(0);
+                    // System.out.println("Servers Check " + servers.toString());
+                    for(String serv: servers){
+                        if(Integer.parseInt(serv.split(" ")[4]) > Integer.parseInt(largestServer.split(" ")[4]) && serv.split(" ")[2].equals("inactive")){
+                            System.out.println("New Largest: " + serv);
+                            largestServer = serv;
+                        }
+                    }
+                    dout.write(("SCHD " + job[2] + " " + largestServer.split(" ")[0] + " " +largestServer.split(" ")[1] +"\n").getBytes());
+                    response = din.readLine();
+                    System.out.println("Response SCHD: " + response);
+                    dout.flush();
+                    while(response.contains(".")){
+                        System.out.println("Ok then");
+                        response = din.readLine();
+                    }
                 }
-            }
-            dout.write(("SCHD " + job[2] + " " + largestServer.split(" ")[0] + " " +largestServer.split(" ")[1] +"\n").getBytes());
-            response = din.readLine();
-            System.out.println("Response SCHD: " + response);
-            dout.flush();
-            }
             }
 
 
