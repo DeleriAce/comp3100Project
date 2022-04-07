2d6e2120be40859cf2fa75c3d5bf5260698d63b8
Kurtis <kurtis.fraser@students.mq.edu.au>
2022-04-07 11:21:58 +1000

    Removed unused code and comments

--- MyClient.java
+++ MyClient.java
@@ -5,16 +5,12 @@ import java.util.ArrayList;
 public class MyClient {
 
     public static ArrayList<ServerObj> largestServers(DataInputStream din, DataOutputStream dout){
-        ArrayList<String> serverList = new ArrayList<String>();
-        ArrayList<String> largestServerList = new ArrayList<String>();
         String response = "";
         int numServers;
-        String largestServer = "";
-
-
         ArrayList<ServerObj> serverList2 = new ArrayList<ServerObj>();
         ArrayList<ServerObj> largestServerList2 = new ArrayList<ServerObj>();
         ServerObj largestServer2 = null;
+
         try {
             //Get number of servers
             dout.write(("GETS All\n").getBytes());
@@ -53,33 +49,6 @@ public class MyClient {
             response = din.readLine();
             System.out.println("Response OK: " + response);
             dout.flush();
-            // Storing Servers as strings
-            // for(int a=0;a<numServers;a++){
-            //     response = din.readLine();
-            //     serverList.add(response);
-            //     if(serverList.indexOf(response) == 0){
-            //         largestServer = response;
-            //     }else{
-            //         //Compare cpu cores
-            //         if(Integer.parseInt(response.split(" ")[4]) > Integer.parseInt(largestServer.split(" ")[4])){
-            //             System.out.println("New Largest: " + response);
-            //             largestServer = response;
-            //         }
-            //     }
-            //     System.out.println("Resp: " + response);
-            // }
-            // // Find all servers with same server type as the largest server
-            // for(String server: serverList){
-            //     if(server.split(" ")[0].equals(largestServer.split(" ")[0])){
-            //         largestServerList.add(server);
-            //     }
-            // }
-
-            // //Confirm Received Server list
-            // dout.write(("OK\n").getBytes());
-            // response = din.readLine();
-            // System.out.println("Response OK: " + response);
-            // dout.flush();
         } catch (Exception e) {
             System.out.println("Failed to get server list");
         }
@@ -139,29 +108,6 @@ public class MyClient {
                 System.out.println("Response REDY: " + response);
                 dout.flush();
             }
-            //Schedule jobs to largest Servers (Servers as Strings)
-            // while(!response.contains("NONE")){
-            //     if(response.contains("JOBN")){
-            //         job = response.split(" ");
-            //         System.out.println("Job: " + response);
-            //         dout.write(("SCHD " + job[2] + " " + listLargestServers.get(count).split(" ")[0] + " " +listLargestServers.get(count).split(" ")[1] +"\n").getBytes());
-            //         response = din.readLine();
-            //         System.out.println("Response SCHD: " + response);
-            //         dout.flush();
-
-            //         // Increment through server list
-            //         if(count+1 >= listLargestServers.size()){
-            //             count = 0;
-            //         }else{
-            //             count++;
-            //         }
-            //     }
-            //     // get new Job
-            //     dout.write(("REDY\n").getBytes());
-            //     response = din.readLine();
-            //     System.out.println("Response REDY: " + response);
-            //     dout.flush();
-            // }
 
             //Exit
             dout.write(("QUIT\n").getBytes());
