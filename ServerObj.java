8ee67f5f1172860a5573ab6261c0ef5d690795a6
Kurtis <kurtis.fraser@students.mq.edu.au>
2022-04-09 10:14:50 +1000

    Added getter and setter functions

--- ServerObj.java
+++ ServerObj.java
@@ -1,15 +1,69 @@
 public class ServerObj{
-    String type, state;
-    Integer id, curStartTime, core, memory, disk;
+    private String serverType, serverState;
+    private Integer serverID, serverCurrStartTime, serverCore, serverMemory, serverDisk;
 
     public ServerObj(String server){
         String[] s = server.split(" ");
-        type = s[0];
-        id = Integer.parseInt(s[1]);
-        state = s[2];
-        curStartTime = Integer.parseInt(s[3]);
-        core = Integer.parseInt(s[4]);
-        memory = Integer.parseInt(s[5]);
-        disk = Integer.parseInt(s[6]);
+        // serverType = s[0];
+        // serverID = Integer.parseInt(s[1]);
+        // serverState = s[2];
+        // serverCurrStartTime = Integer.parseInt(s[3]);
+        // serverCore = Integer.parseInt(s[4]);
+        // serverMemory = Integer.parseInt(s[5]);
+        // serverDisk = Integer.parseInt(s[6]);
+
+        setType(s[0]);
+        setID(Integer.parseInt(s[1]));
+        setState(s[2]);
+        setCurStartTime(Integer.parseInt(s[3]));
+        setCore(Integer.parseInt(s[4]));
+        setMemory(Integer.parseInt(s[5]));
+        setDisk(Integer.parseInt(s[6]));
+    }
+
+    //Setters
+    private void setType(String s){
+        serverType = s;
+    }
+    private void setID(Integer i){
+        serverID = i;
+    }
+    private void setState(String s){
+        serverState = s;
+    }
+    private void setCurStartTime(Integer i){
+        serverCurrStartTime = i;
+    }
+    private void setCore(Integer i){
+        serverCore = i;
+    }
+    private void setMemory(Integer i){
+        serverMemory = i;
+    }
+    private void setDisk(Integer i){
+        serverDisk = i;
+    }
+
+    // Getters
+    public String type(){
+        return serverType;
+    }
+    public Integer id(){
+        return serverID;
+    }
+    public String state(){
+        return serverState;
+    }
+    public Integer currStartTime(){
+        return serverCurrStartTime;
+    }
+    public Integer core(){
+        return serverCore;
+    }
+    public Integer memory(){
+        return serverMemory;
+    }
+    public Integer disk(){
+        return serverDisk;
     }
 }
